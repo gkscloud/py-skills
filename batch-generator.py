3e5df07905cb0cd1c5ca2dff083034404f7cac04
gkscloud <gibran.kichlu@gmail.com>
2020-05-27 21:40:57 -0400

    Create batch-generator.py

---
+++ batch-generator.py
@@ -0,0 +1,9 @@
+# https://stackoverflow.com/questions/8290397/how-to-split-an-iterable-in-constant-size-chunks
+
+def batch(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
+
+for x in batch(range(0, 10), 3):
+    print x
